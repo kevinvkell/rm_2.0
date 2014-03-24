a3187190b27db44c45175fb818d8ebc976b1daa5
kevinvkell <kevin.v.kell@gmail.com>
2014-03-23 22:02:11 -0400

    added renaming functionality if name collisions occur

--- rm.c
+++ rm.c
@@ -10,9 +10,11 @@
 void print_info(char *message);
 void rm_recursive();
 void rm_non_recursive();
+char *name_trash_file();
+char *try_file_name(char* name_to_try, int number_to_try);
 
 int f_flag = 0, h_flag = 0, r_flag = 0;
-char *file_name = NULL;
+char *file_name;
 int current_option;
 char *trash_location;
 
@@ -44,8 +46,9 @@ int main(int argc, char *argv[]) {
 	}
 
 	trash_location = getenv("TRASH");
-	if(!trash_location) {
+	if(trash_location == NULL) {
 		printf("Please set TRASH environment variable\n");
+		exit(1);
 	}
 	else{
 		printf("TRASH: %s \n", trash_location);
@@ -70,18 +73,54 @@ void rm_recursive() {
 
 void rm_non_recursive() {
 	int result;
-	char destination[strlen(trash_location) + strlen(file_name) + 1];
+	char *destination;
 
-	strcat(destination, trash_location);
-	strcat(destination, file_name);
+	destination = name_trash_file();
 
 	printf("destination: %s \n", destination);
 	result = rename(file_name, destination);
+	free(destination);
 	if(result != 0) {
 		perror("rename");
 	}
 	else {
 		printf("Moved %s to trash\n", file_name);
+	}
+}
+
+char *name_trash_file() {
+	char *trash_file_name;
+	trash_file_name = calloc((strlen(trash_location) + strlen(file_name) + 1), sizeof(char));
+	if(trash_file_name == NULL) {
+		printf("could not allocate memory\n");
+		exit(1);
+	}
+
+	strcat(trash_file_name, trash_location);
+	strcat(trash_file_name, file_name);
+	if(access(trash_file_name, F_OK) == 0) {
+		trash_file_name = try_file_name(trash_file_name, 1);
+	}
+
+	return trash_file_name;	
+}
+
+char *try_file_name(char *name_to_try, int number_to_try) {
+	char *new_name_to_try;
+	new_name_to_try = calloc((strlen(name_to_try) + sizeof(int) + 1), sizeof(char));
+	if(new_name_to_try == NULL) {
+		printf("could not allocate memory\n");
+		exit(1);
+	}
+	
+	sprintf(new_name_to_try, "%s.%d", name_to_try, number_to_try);
+
+	if(access(new_name_to_try, F_OK) == 0) {
+		return try_file_name(name_to_try, ++number_to_try);
+	}
+	else {
+		free(name_to_try);
+		return new_name_to_try;
 	}
 }
 
